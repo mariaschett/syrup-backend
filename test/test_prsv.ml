8b83a5d44eea4cefc84cfd254f99cce86517fd8c
Maria A Schett <mail@maria-a-schett.net>
2019-11-12 09:37:32 +0000

    Refactor to sk_init

--- test/test_prsv.ml
+++ test/test_prsv.ml
@@ -6,9 +6,9 @@ open Z3util
 let xs l j = List.init l ~f:(fun i -> Enc.mk_x i j)
 let x's l j = List.init l ~f:(fun i -> Enc.mk_x i (j+1))
 
-let sk_init l j vals =
+let sk_init j vals =
   let open Z3Ops in
-  conj (List.map2_exn (xs l j) vals ~f:(fun x v -> x == v))
+  conj (List.mapi vals ~f:(fun i v -> Enc.mk_x i j == v))
 
 let prsv =
   [
@@ -16,7 +16,7 @@ let prsv =
         let j = 2 in
         let vals = [num 1; num 2;] in
         let l = List.length vals in
-        let c = sk_init l j vals in
+        let c = sk_init j vals in
         let m = solve_model_exn [c] in
         assert_equal
           ~cmp:[%eq: Z3.Expr.t list]
@@ -29,7 +29,7 @@ let prsv =
         let k = 4 and j = 2 in
         let vals = [num 1; num 2;] in
         let l = List.length vals in
-        let c = sk_init l j vals in
+        let c = sk_init j vals in
         let c' = Enc.enc_prsv_all_from k 0 j in
         let m = solve_model_exn [c; c'] in
         assert_equal
@@ -43,7 +43,7 @@ let prsv =
         let k = 4 and j = 2 in
         let vals_prsv = [num 3; num 4;] in
         let vals_chng = [num 1; num 2;] in
-        let c = sk_init k j (vals_chng @ vals_prsv) in
+        let c = sk_init j (vals_chng @ vals_prsv) in
         let c' = Enc.enc_prsv_all_from k 2 j in
         let m = solve_model_exn [c; c'] in
         assert_equal
@@ -56,7 +56,7 @@ let prsv =
     "Stack is preserved moving one element up from index 1">:: (fun _ ->
         let k = 4 and j = 2 in
         let vals = [num 1; num 2; num 3;] in
-        let c = sk_init (List.length vals) j vals in
+        let c = sk_init  j vals in
         let c' = Enc.enc_prsv_move_up_from k 1 j in
         let m = solve_model_exn [c; c'] in
         assert_equal
@@ -69,7 +69,7 @@ let prsv =
     "Stack is preserved moving one element down from index 0">:: (fun _ ->
         let k = 4 and j = 2 in
         let vals = [num 1; num 2; num 3;] in
-        let c = sk_init (List.length vals) j vals in
+        let c = sk_init j vals in
         let c' = Enc.enc_prsv_move_down k 0 j in
         let m = solve_model_exn [c; c'] in
         assert_equal
