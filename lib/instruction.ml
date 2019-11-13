ea0c75faaed0041cc97b061c39ca4af7dcf1323b
Maria A Schett <mail@maria-a-schett.net>
2019-11-13 11:49:09 +0000

    Add alpha_delta function

--- lib/instruction.ml
+++ lib/instruction.ml
@@ -1,3 +1,4 @@
+open Core
 
 type t =
     SWAP
@@ -18,3 +19,19 @@ let enc iota =
     | DUP -> 4
     | NOP -> 5
   in Z3util.num i
+
+let alpha_delta = function
+  | SWAP -> (2,2)
+  | PUSH -> (0,1)
+  | ADD -> (2,1)
+  | POP -> (1,0)
+  | DUP -> (1,2)
+  | NOP -> (0,0)
+
+let diff iota =
+  let (alpha, delta) = alpha_delta iota in
+  (alpha - delta)
+
+let alpha iota = alpha_delta iota |> Tuple.T2.get1
+
+let delta iota = alpha_delta iota |> Tuple.T2.get2
