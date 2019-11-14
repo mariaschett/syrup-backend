ffb93a3b7fc4d8df51f91ec8f2d88ed92c06efdd
Maria A Schett <mail@maria-a-schett.net>
2019-11-14 17:13:30 +0000

    Use generic sk_utlz

--- lib/enc.ml
+++ lib/enc.ml
@@ -29,6 +29,8 @@ let enc_sk_utlz_unchanged k j = enc_sk_utlz_shft k j 0
 (* no effect if u_0_j is not utilized *)
 let enc_sk_utlz_rm k j delta = enc_sk_utlz_shft k j (-delta)
 
+let enc_sk_utlz k j iota = enc_sk_utlz_shft k j (-(diff iota))
+
 (* preserve *)
 
 let enc_prsv_from_diff diff k l j =
@@ -49,12 +51,12 @@ let enc_push k j =
   let a = mk_a j in
   let open Z3Ops in
   ~! u_k &&
-  (x'_0 == a && enc_prsv k j (PREDEF PUSH) && enc_sk_utlz_add k j 1)
+  (x'_0 == a && enc_prsv k j (PREDEF PUSH) && enc_sk_utlz k j (PREDEF PUSH))
 
 let enc_pop k j =
   let u_0 = mk_u 0 j in
   let open Z3Ops in
-  u_0 && (enc_prsv k j (PREDEF POP) && enc_sk_utlz_rm k j 1)
+  u_0 && (enc_prsv k j (PREDEF POP) && enc_sk_utlz k j (PREDEF POP))
 
 let enc_swap k j =
   let x_0 = mk_x 0 j and x'_0 = mk_x' 0 j in
@@ -62,7 +64,7 @@ let enc_swap k j =
   let u_0 = mk_u 0 j and u_1 = mk_u 1 j in
   let open Z3Ops in
   u_0 && u_1 &&
-  ((x'_0 == x_1) && (x'_1 == x_0)) && enc_prsv k j (PREDEF SWAP) && enc_sk_utlz_unchanged k j
+  ((x'_0 == x_1) && (x'_1 == x_0)) && enc_prsv k j (PREDEF SWAP) && enc_sk_utlz k j (PREDEF SWAP)
 
 let enc_dup k j =
   let x_0 = mk_x 0 j and x'_0 = mk_x' 0 j in
@@ -70,11 +72,11 @@ let enc_dup k j =
   let u_0 = mk_u 0 j and u_l = mk_u (k-1) j in
   let open Z3Ops in
   u_0 && ~! u_l &&
-  ((x'_0 == x_0) && (x'_1 == x_0) && enc_prsv k j (PREDEF DUP) && enc_sk_utlz_add k j 1)
+  ((x'_0 == x_0) && (x'_1 == x_0) && enc_prsv k j (PREDEF DUP) && enc_sk_utlz k j (PREDEF DUP))
 
 let enc_nop k j =
   let open Z3Ops in
-  enc_prsv k j (PREDEF NOP) && enc_sk_utlz_unchanged k j
+  enc_prsv k j (PREDEF NOP) && enc_sk_utlz k j (PREDEF NOP)
 
 let effect k enc_userdef iota j =
   match iota with
