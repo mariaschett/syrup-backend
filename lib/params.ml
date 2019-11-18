5b2354bcc7a442610e2ed8446052a7ec7d0d725d
Maria A Schett <mail@maria-a-schett.net>
2019-11-18 14:45:07 +0000

    Change output to show only name and number

--- lib/params.ml
+++ lib/params.ml
@@ -1,4 +1,5 @@
 open Core
+open Instruction
 
 type params = {
   instrs : Instruction.t list;
@@ -12,7 +13,7 @@ let mk instrs =
 let enc_instr_name params iota =
   List.Assoc.find_exn (params.map) iota ~equal:(fun i1 i2 -> i1.name = i2.name) |> Z3util.num
 
-let show_map params = [%show: (Instruction.t * int) list] params.map
+let show_map params = [%show: (string * int) list] (List.map params.map ~f:(fun (iota, i) -> (iota.name, i)))
 
 let nop_enc_name params =
   let (_, i) = List.find_exn (params.map) ~f:(fun (iota, _) -> iota.name = "NOP")
