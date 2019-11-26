6bff535a1c08f732defb202691356d15b24ce9b9
Maria A Schett <mail@maria-a-schett.net>
2019-11-26 16:23:34 +0000

    Extract function to encode for all vars

--- lib/enc.ml
+++ lib/enc.ml
@@ -34,16 +34,18 @@ let enc_block params =
   && nop_propagate params
   && bounds_push_args params
 
+let for_all_vars params ss =
+  let fresh_num i = bignum (Z.add params.max_wsz (Z.of_int i)) in
+  let open Z3Ops in
+  conj (List.mapi ss ~f:(fun i s -> s == fresh_num i))
+
 let enc_block_192 params =
   let s_1 = mk_s 1 (* =^= ADD_1 *) in
   let sk_x = Z3util.intconst ("sk_x") (* =^= input variable on stack *) in
   let source_sk = sk_init params.k 0 [sk_x] in
   let target_sk = sk_init params.k (params.n-1) [s_1; num 146] in
-  let fresh_num i = bignum (Z.add params.max_wsz (Z.of_int i)) in
-  let all_vars =
-    conj (List.mapi [s_1; sk_x] ~f:(fun i s -> s <==> fresh_num i)) in
   let open Z3Ops in
-  source_sk && target_sk && enc_block params && all_vars
+  source_sk && target_sk && enc_block params && (for_all_vars params [s_1; sk_x])
 
 let enc_block_ex1 params =
   let source_sk = sk_init params.k 0 [] in
@@ -55,8 +57,5 @@ let enc_block_ex2 params =
   let sk_x = Z3util.intconst ("sk_x") in
   let source_sk = sk_init params.k 0 [sk_x] in
   let target_sk = sk_init params.k (params.n-1) [sk_x; sk_x] in
-  let fresh_num i = bignum (Z.add params.max_wsz (Z.of_int i)) in
-  let all_vars =
-    conj (List.mapi [sk_x] ~f:(fun i s -> s <==> fresh_num i)) in
   let open Z3Ops in
-  source_sk && target_sk && enc_block params && all_vars
+  source_sk && target_sk && enc_block params && (for_all_vars params [sk_x])
