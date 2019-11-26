25b6346aca65c3613665c6065098de0e26303194
Maria A Schett <mail@maria-a-schett.net>
2019-11-26 10:46:38 +0000

    Update map of instructions to be out of bounds of PUSH args

--- lib/params.ml
+++ lib/params.ml
@@ -3,7 +3,7 @@ open Instruction
 
 type params = {
   instrs : Instruction.t list;
-  map : (Instruction.t * int) list;
+  map : (Instruction.t * Z.t) list;
   k : int;
   n : int;
   ss : Z3.Expr.expr list;
@@ -12,13 +12,15 @@ type params = {
 
 let mk ~k:k ~n:n ~forall:ss instrs =
   let max_wsz = Z.pow (Z.of_int 2) 256 in
-  let map = List.mapi instrs ~f:(fun i iota -> (iota, i)) in
+  let map = List.mapi instrs ~f:(fun i iota -> (iota, Z.add max_wsz (Z.of_int i))) in
   { instrs = instrs; map = map; k = k; n = n; ss = ss; max_wsz = max_wsz }
 
 let enc_instr_name params iota =
-  List.Assoc.find_exn (params.map) iota ~equal:(fun i1 i2 -> i1.name = i2.name) |> Z3util.num
+  List.Assoc.find_exn (params.map) iota ~equal:(fun i1 i2 -> i1.name = i2.name) |> Z3util.bignum
 
-let show_map params = [%show: (string * int) list] (List.map params.map ~f:(fun (iota, i) -> (iota.name, i)))
+let show_map params =
+  let show_pair (iota, i) = iota.name ^ " : " ^ Z.to_string i ^ "; " in
+  List.fold params.map ~init:"" ~f:(fun m im -> m ^ (show_pair im))
 
 let nop_enc_name params =
   let (_, i) = List.find_exn (params.map) ~f:(fun (iota, _) -> iota.name = "NOP")
