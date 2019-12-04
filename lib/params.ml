be643df500c9075ba198fcec30de9b3ea468cbec
Maria A Schett <mail@maria-a-schett.net>
2019-12-04 07:58:16 +0000

    Rename

--- lib/params.ml
+++ lib/params.ml
@@ -3,7 +3,7 @@ open Instruction
 
 type params = {
   instrs : Instruction.t list;
-  map : (Instruction.t * int) list;
+  instr_int_map : (Instruction.t * int) list;
   k : int;
   n : int;
   max_wsz : Z.t;
@@ -12,11 +12,11 @@ type params = {
   ss : Z3.Expr.expr list; (* "forall quantified" variables *)
 }
 
-let mk ~k:k ~n:n ~src_ws:src_ws ~tgt_ws:tgt_ws ~ss:ss instrs =
+let mk ~k ~n ~src_ws ~tgt_ws ~ss instrs =
   let max_wsz = Z.pow (Z.of_int 2) 10 in
   let map = List.mapi instrs ~f:(fun i iota -> (iota, i)) in
   { instrs = instrs;
-    map = map;
+    instr_int_map = map;
     k = k;
     n = n;
     max_wsz = max_wsz;
@@ -26,13 +26,13 @@ let mk ~k:k ~n:n ~src_ws:src_ws ~tgt_ws:tgt_ws ~ss:ss instrs =
   }
 
 let enc_instr_name params iota =
-  List.Assoc.find_exn (params.map) iota ~equal:(fun i1 i2 -> i1.id = i2.id) |> Z3util.num
+  List.Assoc.find_exn (params.instr_int_map) iota ~equal:(fun i1 i2 -> i1.id = i2.id) |> Z3util.num
 
 let show_map params =
   let show_pair (iota, i) = iota.id ^ " : " ^ [%show: int] i ^ "; " in
-  List.fold params.map ~init:"" ~f:(fun m im -> m ^ (show_pair im))
+  List.fold params.instr_int_map ~init:"" ~f:(fun m im -> m ^ (show_pair im))
 
 let enc_instr params id =
-  List.find_exn (params.map) ~f:(fun (iota, _) -> iota.id = id) |> Tuple.T2.get2
+  List.find_exn (params.instr_int_map) ~f:(fun (iota, _) -> iota.id = id) |> Tuple.T2.get2
 
 let nop_enc_name params = enc_instr params "NOP"
