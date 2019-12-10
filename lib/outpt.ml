0dbd9aa6b9d61f31ea2ddc651535ad72673b6848
Maria A Schett <mail@maria-a-schett.net>
2019-12-10 16:31:55 +0100

    Rename argument

--- lib/outpt.ml
+++ lib/outpt.ml
@@ -6,8 +6,6 @@ let show_smt enc =
   (* hack get model *)
   (Z3.Optimize.to_string !octxt) ^  "(get-model)"
 
-
-let write_smt_and_map fn ex params =
-  let ex' = show_smt ex in
-  Out_channel.write_all (fn^".smt") ~data:ex';
+let write_smt_and_map fn cnstrs params =
+  Out_channel.write_all (fn^".smt") ~data:(show_smt cnstrs);
   Out_channel.write_all (fn^".map") ~data:(Params.show_instr_to_int params)
