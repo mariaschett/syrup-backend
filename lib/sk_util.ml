d70ee71db0bf6c61d1943a000fb4783d35c9cf16
Maria A Schett <mail@maria-a-schett.net>
2019-12-31 17:39:06 +0000

    Refactor

--- lib/sk_util.ml
+++ lib/sk_util.ml
@@ -18,28 +18,43 @@ let enc_sk_utlz_init k l =
   let open Z3Ops in
   conj (List.init k ~f:(fun i -> u i == is_utlzd i))
 
-let enc_sk_utlz k j ~alpha ~delta =
-  let diff = alpha - delta in
-  let u' i = mk_u' i j in
-  let u i = mk_u i j in
-  let shft i =
-    if diff >= 0
-    then if i < diff then top else u (i-diff)
-    else if i >= k - (abs(diff)) then btm else u (i+(abs(diff)))
-  in
-  let open Z3Ops in
-  conj (List.init k ~f:(fun i -> u' i == shft i))
+let idx_to_add alpha =
+  List.range ~start:`inclusive 0 ~stop:`exclusive alpha
 
-(* preserve *)
+let idx_to_del k alpha delta =
+  List.range ~start:`inclusive (k-delta+alpha) ~stop:`exclusive k
 
 let idxs_to_shift k alpha delta =
   (* to avoid generating x_l_j for l >= k *)
   let k = if alpha > delta then k - alpha + delta else k in
   List.range ~start:`inclusive delta ~stop:`exclusive k
 
+let enc_sk_utlz_add j ~alpha =
+  let u' i = mk_u' i j in
+  let open Z3Ops in
+  conj (List.map (idx_to_add alpha) ~f:(fun i -> u' i == top))
+
+let enc_sk_utlz_prsv k j ~alpha ~delta =
+  let u i = mk_u i j and u' i = mk_u' (i-delta+alpha) j in
+  let open Z3Ops in
+  conj (List.map (idxs_to_shift k alpha delta) ~f:(fun i -> u' i == u i))
+
+let enc_sk_utlz_del k j ~alpha ~delta =
+  let u' i = mk_u' i j in
+  let open Z3Ops in
+  conj (List.map (idx_to_del k alpha delta) ~f:(fun i -> u' i == btm))
+
+let enc_sk_utlz k j ~alpha ~delta =
+  let open Z3Ops in
+  enc_sk_utlz_add j ~alpha &&
+  enc_sk_utlz_prsv k j ~alpha ~delta &&
+  enc_sk_utlz_del k j ~alpha ~delta
+
+(* preserve *)
+
 let enc_prsv k j ~alpha ~delta =
   let u i = mk_u i j in
-  let x i = mk_x i j and x' i = mk_x' (i - delta + alpha) j in
+  let x i = mk_x i j and x' i = mk_x' (i-delta+alpha) j in
   let idx = idxs_to_shift k alpha delta in
   let open Z3Ops in
   conj (List.map idx ~f:(fun i -> u i ==> (x' i == x i)))
