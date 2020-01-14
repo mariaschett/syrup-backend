e9974b0d01601a34c40a8b266b7bc84a690ba754
Maria A Schett <mail@maria-a-schett.net>
2020-01-14 16:22:34 +0000

    Refactor

--- lib/outpt.ml
+++ lib/outpt.ml
@@ -283,7 +283,29 @@ let show_csv rslt omit_csv_header slvr_time_in_sec =
   (if omit_csv_header then "" else show_csv_header) ^
   (String.concat ~sep:"," data)
 
+(* pretty print target program *)
+
+let show_disasm params ~dec_instr ~dec_arg =
+  let arg iota i = Option.some_if (Instruction.is_PUSH iota) (dec_arg i) in
+  List.init params.n ~f:(fun i ->
+      let iota = dec_instr i in
+      Instruction.show_disasm iota ~arg:(arg iota i)
+    )
+
+let show_opcode params ~dec_instr ~dec_arg =
+  let arg iota i = Option.some_if (Instruction.is_PUSH iota) (dec_arg i) in
+  List.init params.n ~f:(fun i ->
+      let iota = dec_instr i in
+      Instruction.show_opcode iota ~arg:(arg iota i)
+    )
+
 (* pretty print from internal model *)
+
+type trgt_prgrm = {
+  opcode : string;
+  disasm : string;
+  cost : string;
+} [@@deriving yojson]
 
 let dec_arg mdl i =
   let a_i = Z3util.eval_const mdl (mk_a i) in
@@ -293,28 +315,16 @@ let dec_instr mdl params i =
   let t_i = Z3util.eval_const mdl (mk_t i) in
   instr_of_int params (Big_int.int_of_big_int (Z3.Arithmetic.Integer.get_big_int t_i))
 
-let show_disasm mdl params =
-  let arg iota i = Option.some_if (Instruction.is_PUSH iota) (dec_arg mdl i) in
-  List.init params.n ~f:(fun i ->
-      let iota = dec_instr mdl params i in
-      Instruction.show_disasm iota ~arg:(arg iota i)
-    )
+let show_disasm_mdl mdl params = show_disasm params
+    ~dec_instr:(dec_instr mdl params)
+    ~dec_arg:(dec_arg mdl)
 
-let show_opcode mdl params =
-  let arg iota i = Option.some_if (Instruction.is_PUSH iota) (dec_arg mdl i) in
-  List.init params.n ~f:(fun i ->
-      let iota = dec_instr mdl params i in
-      Instruction.show_opcode iota ~arg:(arg iota i)
-    )
-
-type trgt_prgrm = {
-  opcode : string;
-  disasm : string;
-  cost : string;
-} [@@deriving yojson]
+let show_opcode_mdl mdl params = show_opcode params
+    ~dec_instr:(dec_instr mdl params)
+    ~dec_arg:(dec_arg mdl)
 
 let show_trgt_prgrm mdl obj params =
-  { opcode = [%show: string list] (show_opcode mdl params);
-    disasm = [%show: string list] (show_disasm mdl params);
+  { opcode = [%show: string list] (show_opcode_mdl mdl params );
+    disasm = [%show: string list] (show_disasm_mdl mdl params);
     cost = [%show: int] (Z3util.solve_objectives mdl obj);
   }
